91d14d92ff14f1c6e3f649c4685aeeec12dcdd44
brentreynolds <bsreynol@iu.edu>
2022-11-28 08:40:33 -0500

    Initial commit

---
+++ Assignment6_BSR/src/DepthFirstSearch.java
@@ -0,0 +1,18 @@
+
+public class DepthFirstSearch {
+Node root;
+
+public DepthFirstSearch() {
+	System.out.println("Call DFS with root node to do a Depth First Search.");
+}
+
+public void DFS(Node node) {
+	System.out.println(node.getData());
+	//Add code to traverse the tree here
+	//This is approximately three lines of code
+	//Including an if statement to see if the node is a leaf
+	//And two recursive calls to the children
+	
+	//Modify other code as necessary
+}
+}
